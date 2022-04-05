3a00b83fbf9f0b838d43eb4ddedb09e2749ca9e0
Michael Altamirano <michaeljaltamirano@gmail.com>
2022-04-05 11:18:50 -0700

    Add back cypress config

---
+++ src/configs/cypress/index.js
@@ -0,0 +1,46 @@
+const { importOrderBaseConfig } = require("../shared");
+
+const cypress = {
+  extends: ["plugin:cypress/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["cypress"],
+  rules: {
+    "cypress/no-assigning-return-values": 1,
+    "cypress/no-unnecessary-waiting": 1,
+    "cypress/no-async-tests": 2,
+    "cypress/assertion-before-screenshot": 2,
+    "cypress/require-data-selectors": 0,
+    "cypress/no-force": 1,
+    "cypress/no-pause": 2,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    camelcase: 0,
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-unused-vars": [
+      1,
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "import/order": [
+      2,
+      {
+        ...importOrderBaseConfig,
+        pathGroups: [
+          {
+            pattern: "{constants,integration,fixtures}/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: [],
+      },
+    ],
+    "no-console": "off", // `cypress-log-to-output` makes console usage useful for debugging
+  },
+  env: {
+    "cypress/globals": true,
+  },
+};
+
+module.exports = cypress;
---
+++ src/configs/cypress/test.js
@@ -0,0 +1,8 @@
+const cypress = require(".");
+const { generateTest } = require("../tests/utils");
+
+describe("eslint-plugin-curology", () => {
+  describe("cypress config", () => {
+    generateTest(__dirname, cypress);
+  });
+});
